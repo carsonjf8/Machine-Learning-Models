bd462ecbd1d89d80589893c57825cea94e1208da
carsonjf8 <carsonjf8@gmail.com>
2024-01-23 21:58:07 -0500

    updated knn documentation

--- models/knn.py
+++ models/knn.py
@@ -21,14 +21,30 @@ class KNN:
         elif distance_metric == 'minkowski':
             self.distance_metric = lambda x1, x2: metrics.minkowski_distance(x1, x2, p)
 
+    '''
+    store training data that will be used for predictions
+    x_train: NumPy Array
+        training data
+    y_train: NumPy Array
+        training labels
+    '''
     def train(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
 
+    '''
+    predict classes for test data
+    x_test: NumPy Array
+        data for which to predict classes
+    '''
     def predict(self, x_test):
+        # compute distances between training and test data
         distances = self.distance_metric(np.expand_dims(self.x_train, axis=0), np.expand_dims(x_test, axis=1))
         sorted_indices = np.argsort(distances, axis=1)
+        # get closest neighbors
         closest_neighbors = sorted_indices[:, :self.num_neighbors]
+        # get class votes for each data point
         neighbor_classes = self.y_train[closest_neighbors]
+        # assign to class with most votes
         predicted_classes = stats.mode(neighbor_classes, axis=1).mode
         return predicted_classes
